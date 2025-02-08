a83332800faa99579e233ac4577b4c16df5a0eba
Tushar <tusharpatel1093@gmail.com>
2025-02-08 10:31:40 +0530

    Add Another Question

--- Tutorials.py
+++ Tutorials.py
@@ -107,7 +107,7 @@ print(
 
 
 # 7. Write a program to input two numbers as input and compute the greatest common divisor(GCD).
-
+# Method 1
 a = int(input())
 b = int(input())
 while b != 0:
@@ -115,6 +115,16 @@ while b != 0:
     b = a % b
     a = temp
 print(a)
+
+# or
+
+# Method 2
+import math
+
+n1 = int(input())
+n2 = int(input())
+print(f"The GCD of {n1} and {n2} is:", end=" ")
+print(math.gcd(n1, n2))
 
 
 # 8. Calculate Area of triangle by user input base & height in python
@@ -342,6 +352,7 @@ print(f"The two value of Division => {num1} / {num2} =", Div)
 
 # 27. Write a python program to find the sum of digits of an integer number using while loop.
 
+# Method 1
 n = 654
 sum = 0
 while n > 0:
@@ -350,6 +361,23 @@ while n > 0:
     n = n // 10
 
 print("The sum of digits if :", sum)
+
+# or
+
+
+# Method 2
+def sumdigit(n):
+
+    sum = 0
+    while n > 0:
+        p = n % 10
+        sum = sum + p
+        n = n // 10
+    return sum
+
+
+n = int(input("Enter a number: "))
+print("The sum of digits if :", sumdigit(n))
 
 
 # * reverse number.
@@ -429,6 +457,27 @@ for char in string:
 for char in dictionary:
     print(char, "=>", dictionary[char])
 
+# 32. Write a python program to fine the odd numbers in an array.abs
+# Method 1
+import numpy as np
+
+array = np.array([10, 25, 30, 65, 75, 50, 121])
+
+print("**The List of Odd Numbers in this oddArray Array")
+for i in range(len(array)):
+    if array[i] % 2 != 0:
+        print(array[i], end=" ")
+
+# or
+
+# Method 2
+list = [10, 25, 30, 65, 75, 50, 121]
+
+print("**The List of Odd Numbers in this oddArray Array")
+for i in range(len(list)):
+    if list[i] % 2 != 0:
+        print(list[i], end=" ")
+
 
 # * Pattern
 for i in range(6, 0, -1):
@@ -437,62 +486,319 @@ for i in range(6, 0, -1):
     print()
 
 
-# 32. Advanced print method
+# 33. Advanced print method
 message = input("enter a message: ")
 print("Hello", message)
 
+# 34. String have a special character or not
+string = input("Enter a string: ")
+if any(not char.isalnum() for char in string):
+    print("String is not accepted")
+else:
+    print("String is accepted")
 
+
+# 35. Write a python program 2020 calender (1,1,2020)-(1,12,2020) print january 1,2020 is Wednesday. Note That 2020 is leap year.
+import calendar
+
+year = 2020
+print("January 1, 2020 is Wednesday.\n")
+for month in range(1, 13):
+    print(calendar.month(year, month))
+
+
+# 36. Write a python to print Fibonacci seris by resucrsion.
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return recur_fibo(n - 1) + recur_fibo(n - 2)
+
+
+nterms = int(input("How many terms ? : "))
+if nterms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(nterms):
+        print(recur_fibo(i), end=" ")
+
+
+# 37. Write a python program to given string in a new string no duplicate consecutive latters.
+# input: Pythhonn  Output: Python
+def remove_dup(input_string):
+    result = input_string[0]
+    for i in range(1, len(input_string)):
+        if input_string[i] != input_string[i - 1]:
+            result += input_string[i]
+    return result
+
+
+input_string = input("Enter the STRING : ")
+print(remove_dup(input_string))
+
+
+# 38. Write a python program to calculate the sum of all even number (first 100 even number in a list)
+list = [x for x in range(2, 202, 2)]
+print(list)
+sum_of_even_number = sum(list)
+print(sum_of_even_number)
+
+# 39. Write a python program to calculate the sum of all odd number (first 100 even number in a list)
+list = [x for x in range(1, 201, 2)]
+print(list)
+sum_of_even_number = sum(list)
+print(sum_of_even_number)
+
+# 40. Write a python program to print all the odd number (First 100 odd number in a ascending order)
+list = [x for x in range(1, 201, 2)]
+list.sort()
+print(list)
+
+
+# 41. Write a python program name of 10 students and input students marks. Find the maximum and minimum marks display their student names.
+student = {}
+for _ in range(4):
+    name = input("Enter Student name : ")
+    marks = int(input("Enter marks : "))
+    student[name] = marks
+max_student = max(student, key=student.get)
+max_marks = student[max_student]
+min_student = min(student, key=student.get)
+min_marks = student[min_student]
+print(f"Student with maximum marks is {max_student} with {max_marks} marks")
+print(f"Student with minimum marks is {min_student} with {min_marks} marks")
+
+# 42. Write a python programming to find the area of circle and rectangle Take the input from the user.
+radius = float(input())
+area_circle = 3.14 * radius**2
+print("Area of the circle : ", area_circle)
+length = float(input())
+width = float(input())
+area_rectangle = length * width
+print("Area of the rectangle : ", area_rectangle)
+
+
+# 43. Write a python program in a given list of find positive and negative number.
+# input = [-4,3,-81,13,5,-23,-52]
+# output : positive = 3, negative = 4
+list = [-4, 3, -81, 13, 5, -23, -52]
+positive_count = sum(1 for num in list if num > 0)
+negative_count = sum(1 for num in list if num < 0)
+print("Positive number is : ", positive_count)
+print("Negative number is : ", negative_count)
+
+
+# 44. Write a python program concatenate two 3 dimensional numpy array on axis 1.
+import numpy as np
+
+array1 = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+array2 = np.array([[[9, 10], [11, 12]], [[13, 14], [15, 16]]])
+result = np.concatenate((array1, array2), axis=1)
+print("Array 1 : ", array1)
+print("\nArray 2 : ", array2)
+print("Concatenate Array : ", result)
+
+
+# 45. Write a python program to count even odd number in a list using lambda function.
+numbers = [1, 7, 9, 4, 2, 6, 5, 8, 3, 10]
+count_even = len(list(filter(lambda x: x % 2 == 0, numbers)))
+count_odd = len(list(filter(lambda x: x % 2 != 0, numbers)))
+print("Even number count is : ", count_even)
+print("Even number count is : ", count_odd)
+
+
+# 46. Write a python program to find key with maximum unique value in a dictionary
+# tet_dict={"A":[4,5,6,3],"B":[7,5,9,0],"C":[1,5,9,2]}
+
+test_dict = {"A": [4, 5, 6, 3], "B": [7, 5, 9, 0], "C": [1, 5, 9, 2]}
+max_unique_count = 0
+key_with_max_unique = None
+for key, value in test_dict.items():
+    unique_count = len(set(value))
+    if unique_count > max_unique_count:
+        max_unique_count = unique_count
+        key_with_max_unique = key
+
+print(f"key with maximum unique value: ", {key_with_max_unique})
+
+
+# 47. Write a program to replace 'a' with 'b', 'b' with 'c',...,'z' with 'a' similarly for 'A' with 'B', 'B' with 'C',...,'Z' with 'A' in a file. The other characters should remain unchanged.
+def replace_chars(input_string):
+    output_string = ""
+    for char in input_string:
+        if char.isalpha():
+            if char == "z":
+                output_string += "a"
+            else:
+                output_string += chr(ord(char) + 1)
+        else:
+            output_string += char
+    return output_string
+
+
+str = "A B C D E F"
+print(str)
+print(replace_chars(str))
+
+
+# 48. Write a function that reads the contents of the file f3.txt and counts the numbers of alphabets, black spaces, lowercase, numbers of words startng with a vowel and number of occurences of a word "hello"
+def read_and_count():
+    lower_case = 0
+    uper_case = 0
+    alphabets = 0
+    space = 0
+    digit = 0
+    string_count = 0
+    with open("./demo.txt") as demo:
+        c = demo.readlines()
+    for j in c:
+        for i in range(len(j)):
+            if j[i].isalpha():
+                alphabets += 1
+            if j[i].isdigit:
+                digit += 1
+            if j[i].islower():
+                lower_case += 1
+            if j[i].isupper():
+                uper_case += 1
+            if k[i] == " ":
+                space += 1
+        string_count += j.lower().count("hello")
+    print("Alphabets : ", alphabets)
+    print("Space : ", space)
+    print("Digits : ", digit)
+    print("Lower Case : ", lower_case)
+    print("Upper Case : ", uper_case)
+    print("Count of 'hello' : ", string_count)
+
+
+read_and_count()
+
+
+# 49. Write a python function that takes two list and returns True if they have at least one common item.abs
+def common_data(list1, list2):
+    result = False
+    for x in list1:
+        for y in list2:
+            if x == y:
+                result = True
+                return result
+
+
+print(common_data([1, 2, 3, 4, 5], [5, 6, 7, 8, 9]))
+print(common_data([1, 2, 3, 4, 5], [6, 7, 8, 9]))
+
+
+# 50. Write a NumPy Program to find the most frequent value in an array
+import numpy as np
+
+x = np.array([1, 2, 3, 4, 5, 1, 2, 1, 1, 1])
+print("Original Array :", x)
+print("Most Frequent value in the above array :")
+print(np.bincount(x).argmax())
+
+# 51. Write a python program for Row-wise element Addition in Tuple Matrix.
+tuple_matrix = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+row_sums = []
+for row in tuple_matrix:
+    row_sums.append(sum(row))
+for row in tuple_matrix:
+    print(row)
+print("Row-wise :", row_sums)
+
+# 52. Write a program to count the number of charactor (charactor frequency) in a string "google.com".
+string = "google.com"
+freq_count = {}
+for char in string:
+    if char in freq_count:
+        freq_count[char] += 1
+    else:
+        freq_count[char] = 1
+print(freq_count)
 
 # FILE HANDLING
 # 1. Open the File on the Server
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.read())
 
 # 2. Read Only Parts of the File
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.read(7))
 
 # 3. Read Lines
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.readline())
 print(f.readline())
 print(f.readline())
 
 # 4. Read All the Lines
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.readlines())
 
 # 5. Close The file
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.readlines())
 f.close()
 
 # 6. Write to an Existing File
-f=open("Books-Exercise\\demo.txt","a")
+f = open("Books-Exercise\\demo.txt", "a")
 f.write("\nNow the file has more content!")
 f.close()
 
 
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.read())
 
 # 7. overwrite the content
-f=open("Books-Exercise\\demo.txt","w")
+f = open("Books-Exercise\\demo.txt", "w")
 f.write("Woops! I have deleted the content!")
 f.close()
 
 
-f=open("Books-Exercise\\demo.txt","r")
+f = open("Books-Exercise\\demo.txt", "r")
 print(f.read())
 
 
 # 8. Delete a File
 import os
+
 os.remove("demofile.txt")
 
 # 9. Check if File exist
 import os
+
 if os.path.exists("demofile.txt"):
     of.remove("demofile.txt")
 else:
     print("The File does not exist")
+
+
+# 2. Write a program to compute the wages of a daily laborer as per the following rules:
+# Hours worked Rate Applicable Upto first 8 hrs Rs. 100/-
+# a. for next 4hrs Rs. 30/- per hr extra
+# b. for next 4hrs Rs. 40/- per hr extra
+# c. for next 4hrs Rs. 50/- per hr extra
+# d. for rest Rs. 60/- per hr extra
+
+h = int(input("Enter Total worked Hours : "))
+wage = 0
+if h <= 8:
+    wage = 100
+elif h <= 12:
+    wage = 100 + (h - 8) * 30
+elif h <= 16:
+    wage = 220 + (h - 12) * 40
+elif h <= 20:
+    wage = 380 + (h - 16) * 50
+else:
+    wage = 580 + (h - 20) * 60
+print("Your Total wage is =", wage)
+
+# 3. Write a python program to combine two dictionary adding values for common keys.
+from collections import Counter
+
+d1 = {"a": 100, "b": 200, "c": 300}
+d2 = {"a": 100, "b": 200, "d": 400}
+new_dict = Counter(d1) + Counter(d2)
+print(new_dict)
